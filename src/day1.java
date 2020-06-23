28bb4d0965cfea2cc460fee9a431a366949a4ef3
songweicong3 <songweicong3@jd.com>
2020-06-23 18:17:31 +0800

    day1

---
+++ src/day1.java
@@ -0,0 +1,22 @@
+
+/**
+ * 二维数组中的查找
+ * @author songweicong3
+ * @date 2020/6/23 18:15
+ */
+public class day1 {
+    public boolean Find(int target, int [][] array){
+        int i=0;
+        int j=array[0].length-1;
+        while(i<array.length&&j>=0){
+            if(array[i][j]==target)
+                return true;
+            else if(array[i][j]<target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+}
